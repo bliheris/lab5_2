f465c05c52e50badadf8bd9f0cf7dad0ebc5570c
Maciej Bołtromiuk <maciejboltromiuk@gmail.com>
2015-04-27 11:06:49 +0200

    Configured application and added FindPersonByFirstName Test

--- src/main/java/edu/iis/mto/integrationtest/repository/PersonRepository.java
+++ src/main/java/edu/iis/mto/integrationtest/repository/PersonRepository.java
@@ -1,8 +1,11 @@
 package edu.iis.mto.integrationtest.repository;
 
 
+import edu.iis.mto.integrationtest.model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PersonRepository {
+import java.util.List;
 
-
+public interface PersonRepository extends JpaRepository<Person, Long> {
+   List<Person> findByFirstNameLike(String firstName);
 }
--- src/test/java/edu/iis/mto/integrationtest/repository/PersonRepositoryIntegrationTest.java
+++ src/test/java/edu/iis/mto/integrationtest/repository/PersonRepositoryIntegrationTest.java
@@ -8,31 +8,46 @@ import java.util.List;
 import org.junit.Test;
 
 import edu.iis.mto.integrationtest.model.Person;
-
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class PersonRepositoryIntegrationTest extends IntegrationTest {
 
-	
-	private PersonRepository personRepository;
+    @Autowired
+    private PersonRepository personRepository;
 
-	@Test
-	public void testCanAccessDbAndGetTestData() {
-//		List<Person> foundTestPersons = personRepository.findAll();
-//		assertEquals(2, foundTestPersons.size());
-	}
+    @Test
+    public void testCanAccessDbAndGetTestData() {
+        long count = personRepository.count();
+        List<Person> foundTestPersons = personRepository.findAll();
+        assertEquals(count, foundTestPersons.size());
+    }
 
-	@Test
-	public void testSaveNewPersonAndCheckIsPersisted() {
-//		long count = personRepository.count();
-//		personRepository.save(a(person().withId(count + 1)
-//				.withFirstName("Roberto").withLastName("Mancini")));
-//		assertEquals(count + 1, personRepository.count());
-//		assertEquals("Mancini", personRepository.findOne(count + 1)
-//				.getLastName());
-	}
+    @Test
+    public void testSaveNewPersonAndCheckIsPersisted() {
+        long count = personRepository.count();
+        personRepository.save(a(person().withId(count + 1)
+                .withFirstName("Roberto").withLastName("Mancini")));
+        assertEquals(count + 1, personRepository.count());
+        assertEquals("Mancini", personRepository.findOne(count + 1)
+                .getLastName());
+    }
 
-	private Person a(PersonBuilder builder) {
-		return builder.build();
-	}
+    @Test
+    public void findPersonByFirstNameTest() {
+        long count = personRepository.count();
+        long bob1Id = count + 1;
+        long bob2Id = count + 2;
+        personRepository.save(a(person().withId(bob1Id)
+                .withFirstName("Bob").withLastName("Smith")));
+        personRepository.save(a(person().withId(bob2Id)
+                .withFirstName("Bob").withLastName("Potter")));
+
+        List<Person> bobs = personRepository.findByFirstNameLike("Bob");
+        assertEquals(2, bobs.size());
+    }
+
+    private Person a(PersonBuilder builder) {
+        return builder.build();
+    }
 
 }
